b4b4fc0d169771320ba19e44ebdbb54b83930068
ShaiGeorge <shaijokuttikkatt@gmail.com>
2026-01-09 22:04:56 +0530

    feat: hash user password manually on create and edit

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
@@ -13,9 +14,14 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserCrudController extends AbstractCrudController
 {
+    public function __construct(
+        private UserPasswordHasherInterface $userPasswordHasher
+    ) {}
+
     public static function getEntityFqcn(): string
     {
         return User::class;
@@ -40,6 +46,7 @@ class UserCrudController extends AbstractCrudController
         yield TextField::new('password', 'Password')
             ->setFormType(PasswordType::class)
             ->setColumns(12)
+            ->setFormTypeOption('mapped', false) // Prevents the "null" error
             ->setRequired($pageName === Crud::PAGE_NEW)
             ->onlyOnForms();
 
@@ -62,5 +69,33 @@ class UserCrudController extends AbstractCrudController
             ->setLabel('Linked Agency')
             ->setHelp('The agency this user belongs to')
             ->setColumns(12);
+    }
+
+    // Manually Hash Password on Create
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $this->hashPassword($entityInstance);
+        parent::persistEntity($entityManager, $entityInstance);
+    }
+
+    // Manually Hash Password on Edit
+    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $this->hashPassword($entityInstance);
+        parent::updateEntity($entityManager, $entityInstance);
+    }
+
+    private function hashPassword(User $user): void
+    {
+        // Get the plain password from the form manually
+        $request = $this->getContext()->getRequest();
+        $formData = $request->request->all('User');
+        $plainPassword = $formData['password'] ?? null;
+
+        // Only update if the user actually typed a password
+        if (!empty($plainPassword)) {
+            $hashedPassword = $this->userPasswordHasher->hashPassword($user, $plainPassword);
+            $user->setPassword($hashedPassword);
+        }
     }
 }
