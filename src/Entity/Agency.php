e5f26d64c7d3d025d4a8aef2b37283e7c3bdfa09
ShaiGeorge <shaijokuttikkatt@gmail.com>
2026-01-10 05:47:40 +0530

    feat: add tracking fields and foreign keys to database schema
    - Add created_at, updated_at, created_by, and updated_by columns to agency table
    - Add foreign key constraints between related tables including client, policy, user, and others
    - Implement EntityTrackingTrait in Agency entity for tracking changes
    - Provide migration up and down methods to add and remove these schema changes

--- src/Entity/Agency.php
+++ src/Entity/Agency.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use App\Entity\Trait\EntityTrackingTrait;
 use App\Repository\AgencyRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -10,6 +11,8 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: AgencyRepository::class)]
 class Agency
 {
+    use EntityTrackingTrait;
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
