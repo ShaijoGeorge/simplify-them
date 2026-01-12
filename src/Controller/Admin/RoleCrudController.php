a06ceeef7a2046051d1f2855997adc914fa7774f
ShaiGeorge <shaijokuttikkatt@gmail.com>
2026-01-13 01:28:42 +0530

    feat: conditionally set agency field and auto-assign agency on role creation
    - Show agency field as required only for Super Admin users
    - Hide and disable agency field for regular users in the role form
    - Automatically assign current user's agency to the role if not set
    - Override persistEntity to inject agency before saving the role entity
    - Improve role creation UX based on user permissions and context

--- src/Controller/Admin/RoleCrudController.php
+++ src/Controller/Admin/RoleCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Role;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
@@ -39,8 +40,37 @@ class RoleCrudController extends AbstractCrudController
             ->setColumns(12)
             ->setHelp('e.g., "Data Entry Clerk"');
 
-        yield AssociationField::new('agency', 'Agency')
-            ->setColumns(12)
-            ->setRequired(true);
+        // Agency field - conditionally configure based on user role
+        $agencyField = AssociationField::new('agency', 'Agency')
+            ->setColumns(12);
+
+        // Check if user is Super Admin
+        /** @var User $user */
+        $user = $this->getUser();
+        if ($user && in_array('ROLE_SUPER_ADMIN', $user->getRoles())) {
+            // Super Admin: Show the field and make it required
+            yield $agencyField
+                ->setRequired(true)
+                ->setHelp('Super Admin Only: Assign role to a specific agency');
+        } else {
+            // Regular User: Hide the field (will be set automatically)
+            yield $agencyField
+                ->hideOnIndex()
+                ->setDisabled(true);
+        }
+    }
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        if ($entityInstance instanceof Role) {
+            $user = $this->getUser();
+            
+            // Only auto-set agency if the user HAS an agency and didn't manually set one (admin case)
+            if ($user && $user->getAgency() && $entityInstance->getAgency() === null) {
+                $entityInstance->setAgency($user->getAgency());
+            }
+        }
+        
+        parent::persistEntity($entityManager, $entityInstance);
     }
 }
