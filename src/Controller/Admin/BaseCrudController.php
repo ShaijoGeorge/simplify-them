2ada4f7269c585e8b36965c3211bb2be8f4cefcc
Shaijo George <shaijokuttikkatt@gmail.com>
2026-03-06 19:20:59 +0000

    feat: implement agency-based filtering in BaseCrudController's query builder

--- src/Controller/Admin/BaseCrudController.php
+++ src/Controller/Admin/BaseCrudController.php
@@ -2,12 +2,18 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
 use App\Service\PermissionCheckerService;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\QueryBuilder;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 abstract class BaseCrudController extends AbstractCrudController
@@ -52,6 +58,60 @@ abstract class BaseCrudController extends AbstractCrudController
         }
 
         return $actions;
+    }
+
+    public function createIndexQueryBuilder(
+        SearchDto $searchDto,
+        EntityDto $entityDto,
+        FieldCollection $fields,
+        FilterCollection $filters
+    ): QueryBuilder {
+        $qb = parent::createIndexQueryBuilder($searchDto, $entityDto, $fields, $filters);
+
+        /** @var User|null $user */
+        $user = $this->getUser();
+
+        if (!$user || $user->isAdministrator()) {
+            return $qb;
+        }
+
+        $agency = $user->getAgency();
+        if (!$agency) {
+            return $qb->andWhere('1 = 0');
+        }
+
+        $rootAlias = $qb->getRootAliases()[0] ?? 'entity';
+        $em = $this->container->get('doctrine')->getManager();
+        $metadata = $em->getClassMetadata($entityDto->getFqcn());
+
+        if ($metadata->hasAssociation('agency')) {
+            return $qb
+                ->andWhere(sprintf('%s.agency = :agency', $rootAlias))
+                ->setParameter('agency', $agency);
+        }
+
+        if ($metadata->hasAssociation('policy')) {
+            $qb->leftJoin(sprintf('%s.policy', $rootAlias), 'agency_scope_policy');
+            return $qb
+                ->andWhere('agency_scope_policy.agency = :agency')
+                ->setParameter('agency', $agency);
+        }
+
+        if ($metadata->hasAssociation('client')) {
+            $qb->leftJoin(sprintf('%s.client', $rootAlias), 'agency_scope_client');
+            return $qb
+                ->andWhere('agency_scope_client.agency = :agency')
+                ->setParameter('agency', $agency);
+        }
+
+        if ($metadata->hasAssociation('role')) {
+            $qb->leftJoin(sprintf('%s.role', $rootAlias), 'agency_scope_role');
+            return $qb
+                ->andWhere('agency_scope_role.agency = :agency')
+                ->setParameter('agency', $agency);
+        }
+
+        return $qb;
     }
 
     public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
